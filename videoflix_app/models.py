c8fa82c133da6169d5fe7d8e0db9cb63fe8eb550
Linda Österreicher <lindaoest@gmail.com>
2025-06-24 21:19:38 +0200

    Add new model Video & create superuser

--- videoflix_app/models.py
+++ videoflix_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+	title = models.CharField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	video_file = models.FileField(upload_to='videos', blank=True, null=True)
+
+	def __str__(self):
+		return self.title
